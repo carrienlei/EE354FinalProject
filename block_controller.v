d68e343ede0427162c08ae1bca817c09770dfd61
carrienlei <cnlei@usc.edu>
2023-04-20 20:19:18 -0700

    "UPDATE"

--- block_controller.v
+++ block_controller.v
@@ -11,11 +11,18 @@ module block_controller(
    );
 	wire block_fill;
 	wire sand_zone;
+	wire shark1;
+	wire shark2;
+	wire bottle1;
+	wire bottle2;
 	
 	//these two values dictate the center of the block, incrementing and decrementing them leads the block to move in certain directions
 	reg [9:0] xpos, ypos;
+	reg [9:0] shark1xpos, shark1ypos, shark2xpos, shark2ypos, bottle1xpos, bottle1ypos, bottle2xpos, bottle2ypos;
 	
 	parameter RED   = 12'b1111_0000_0000;
+	parameter SHARK =  12'b0000_0101_1000; // 058 grey
+	parameter BOTTLE = 12'b1010_1110_1111; // AEF blue
 	
 	/*when outputting the rgb value in an always block like this, make sure to include the if(~bright) statement, as this ensures the monitor 
 	will output some data to every pixel and not just the images you are trying to display*/
@@ -26,6 +33,14 @@ module block_controller(
 			rgb = RED; 
 		else if (sand_zone == 1)
 			rgb = 12'b1111_1111_0000;
+		else if (shark1)
+			rgb = 12'b0000_0101_1000;
+		else if (shark2)
+			rgb = 12'b0000_0101_1000;
+		else if (bottle1)
+			rgb = 12'b1010_1110_1111;
+		else if (bottle2)
+			rgb = 12'b1010_1110_1111;
 		else	
 			rgb=background;
 	end
@@ -33,7 +48,15 @@ module block_controller(
 	assign block_fill=vCount>=(ypos-5) && vCount<=(ypos+5) && hCount>=(xpos-5) && hCount<=(xpos+5);
 	
 	// assign sand zone
-	assign sand_zone = ((hCount >= 10'd144) && (hCount <= 10'd784)) && ((vCount >= 10'd400) && (vCount <= 10'd475)) ? 1 : 0;
+	assign sand_zone = ((hCount >= 10'd144) && (hCount <= 10'd784)) && ((vCount >= 10'd420) && (vCount <= 10'd490)) ? 1 : 0;
+
+	// assign sharks
+	assign shark1 = ((hCount >= (shark1xpos-10)) && (hCount <= (shark1xpos+10))) && ((vCount >= (shark1ypos-5)) && (vCount <= (shark1ypos+5))) ? 1 : 0;
+	assign shark2 = ((hCount >= (shark2xpos-10)) && (hCount <= (shark2xpos+10))) && ((vCount >= (shark2ypos-5)) && (vCount <= (shark2ypos+5))) ? 1 : 0;
+
+	// assign bottles
+	assign bottle1 = ((hCount >= (bottle1xpos-2)) && (hCount <= (bottle1xpos+2))) && ((vCount >= (bottle1ypos-4)) && (vCount <= (bottle1ypos+4))) ? 1 : 0;
+	assign bottle2 = ((hCount >= (bottle2xpos-2)) && (hCount <= (bottle2xpos+2))) && ((vCount >= (bottle2ypos-4)) && (vCount <= (bottle2ypos+4))) ? 1 : 0;
 
 	always@(posedge clk, posedge rst) 
 	begin
@@ -42,6 +65,15 @@ module block_controller(
 			//rough values for center of screen
 			xpos<=450;
 			ypos<=250;
+			shark1xpos <= 220;
+			shark1ypos <= 135;
+			shark2xpos <= 440;
+			shark2ypos <= 330;
+			bottle1xpos <= 250;
+			bottle1ypos <= 440;
+			bottle2xpos <= 570;
+			bottle2ypos <= 190;
+
 		end
 		else if (clk) begin
 		
@@ -51,6 +83,12 @@ module block_controller(
 			the top left corner corresponds to (hcount,vcount)~(144,35). Which means with a 640x480 resolution, the bottom right corner 
 			corresponds to ~(783,515).  
 		*/
+			shark1xpos <= shark1xpos -3;
+			
+			shark2xpos <= shark2xpos -2;
+			
+			bottle2xpos <= bottle2xpos - 1;
+
 			if(right) begin
 				xpos<=xpos+2; //change the amount you increment to make the speed faster 
 				if(xpos==800) //these are rough values to attempt looping around, you can fine-tune them to make it more accurate- refer to the block comment above
@@ -71,6 +109,20 @@ module block_controller(
 				if(ypos==514)
 					ypos<=34;
 			end
+			
+			if ( ((xpos <= shark1xpos+10)&& (xpos >= shark1xpos-10) && (ypos <= shark1ypos+10) && (ypos >= shark1ypos-10)) || ((xpos <= shark2xpos+10)&& (xpos >= shark2xpos-10) && (ypos <= shark2ypos+10) && (ypos >= shark2ypos-10))) 
+				begin
+					xpos<=450;
+					ypos<=250;
+					shark1xpos <= 220;
+					shark1ypos <= 135;
+					shark2xpos <= 440;
+					shark2ypos <= 330;
+					bottle1xpos <= 250;
+					bottle1ypos <= 440;
+					bottle2xpos <= 570;
+					bottle2ypos <= 190;
+				end
 		end
 	end
 	
