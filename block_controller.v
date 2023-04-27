9a0173cc058181eedceb35ad0548a5e0706f2d79
carrienlei <cnlei@usc.edu>
2023-04-27 13:31:54 -0700

    UPDATE

--- block_controller.v
+++ block_controller.v
@@ -11,6 +11,7 @@ module block_controller(
 	output reg [11:0] background
 	// output q_STILL, q_UP, q_DOWN, q_DONE,
 	// output reg out
+	
    );
 
 	/* reg[3:0] state;
@@ -20,7 +21,7 @@ module block_controller(
 	parameter UP = 3'b001;    // Up state
 	parameter DN = 3'b010;    // Down state
 	parameter DEAD = 3'b011;
-	parameter GAME_OVER = 3'b100;
+	// parameter GAME_OVER = 3'b100;
 	
 	// Define the state register and next state logic
 	reg [1:0] state, next_state;
@@ -51,6 +52,10 @@ module block_controller(
 	parameter SHARK =  12'b0000_0101_1000; // 058 grey
 	parameter BOTTLE = 12'b1010_1110_1111; // AEF blue
 	
+	// diver_rom diver(.clk(clk), .row(row), .col(col), .color_data(color_data));	
+	
+	
+	
 	/*when outputting the rgb value in an always block like this, make sure to include the if(~bright) statement, as this ensures the monitor 
 	will output some data to every pixel and not just the images you are trying to display*/
 	always@ (*) begin
@@ -71,9 +76,10 @@ module block_controller(
 		else	
 			rgb=background_rgb;
 	end
+
 		//the +-5 for the positions give the dimension of the block (i.e. it will be 10x10 pixels)
 	assign block_fill=vCount>=(ypos-5) && vCount<=(ypos+5) && hCount>=(xpos-5) && hCount<=(xpos+5);
-	
+	// assign block_fill = color_data;
 	// assign sand zone
 	assign sand_zone = ((hCount >= 10'd144) && (hCount <= 10'd784)) && ((vCount >= 10'd490) && (vCount <= 10'd520)) ? 1 : 0;
 
@@ -106,7 +112,7 @@ module block_controller(
 		shark1xpos <= shark1xpos - 3;
 		shark2xpos <= shark2xpos - 2;
 		bottle1xpos <= bottle1xpos -2;
-		bottle2xpos <= bottle2xpos - 1;		
+		bottle2xpos <= bottle2xpos - 1;	
 		
 		case (state)
 			IDLE: begin
@@ -117,12 +123,13 @@ module block_controller(
 					next_state = DN;
 				// end else if (bottle_count == 8) begin
 				//	next_state = GAME_OVER;
-			
+				end else if ( ((xpos && shark1xpos) == 1) && ((ypos && shark1ypos) == 1)	) begin
+					next_state = DEAD;
 				end else begin
 					next_state = IDLE;
 				end
-				
 			end
+			
 			UP: begin
 			// out = 1'b1;
 				ypos<=ypos-1;
@@ -135,7 +142,8 @@ module block_controller(
 					next_state = UP;
 				// end else if (bottle_count == 8) begin
 				// 	next_state = GAME_OVER;
-				
+				end else if ( ((xpos && shark1xpos) == 1) && ((ypos && shark1ypos) == 1)	) begin
+					next_state = DEAD;
 				end else begin
 					next_state = IDLE;
 				end
@@ -147,6 +155,8 @@ module block_controller(
 					ypos<=512;
 				if (up) begin
 					next_state = UP;
+				end else if ( ((xpos && shark1xpos) == 1) && ((ypos && shark1ypos) == 1)	) begin
+					next_state = DEAD;
 				end else if (down) begin
 					next_state = DN;
 				//end else if (bottle_count == 8) begin
@@ -156,12 +166,16 @@ module block_controller(
 					next_state = IDLE;
 				end
 			end
-			GAME_OVER: begin
+			/* GAME_OVER: begin
 				background <= 12'b0000_0000_0000;
-			end
+			end */
 			DEAD: begin
 				background <= 12'b1111_1111_0000;
-			end
+				shark1 <= 0;
+				shark2 <=0;
+				bottle1 <=0; 
+				bottle2 <= 0;
+			end 
 		endcase
 		end
 	end
@@ -176,169 +190,10 @@ module block_controller(
 			else if(left)
 				background <= 12'b0000_1111_1111; //turquoise
 			else if(down)
-				background <= 12'b1111_0000_1111; //green
+				background <= 12'b0000_1111_1111; //green
 			else if(up)
 				background <= 12'b0000_1111_1111; //blue
 	end
 	
-	always @(*)
-		if (rst)
-			background <= 12'b0000_0000_0000;
-		else
-		if (((xpos <= shark1xpos+10)&& (xpos >= shark1xpos-10) && (ypos <= shark1ypos+10) && (ypos >= shark1ypos-10)) || ((xpos <= shark2xpos+10)&& (xpos >= shark2xpos-10) && (ypos <= shark2ypos+10) && (ypos >= shark2ypos-10))) begin
-			next_state = DEAD;
-		end
 	
 endmodule
-
-/*
-	always@(posedge clk, posedge rst) 
-	begin
-		if(rst) begin		
-			xpos<=450;
-			ypos<=250;
-			shark1xpos <= 220;
-			shark1ypos <= 135;
-			shark2xpos <= 440;
-			shark2ypos <= 330;
-			bottle1xpos <= 250;
-			bottle1ypos <= 440;
-			bottle2xpos <= 170;
-			bottle2ypos <= 200;
-			state <= q_STILL;
-			end
-		else
-		begin
-			shark1xpos <= shark1xpos - 3;
-			shark2xpos <= shark2xpos - 2;
-			bottle1xpos <= bottle1xpos -2;
-			bottle2xpos <= bottle2xpos - 1;	
-			
-			case(state)
-				q_STILL:
-				begin
-					if (down)
-						state <= q_DOWN;
-					else if (up)
-						state <= q_UP;
-					else if (!(up) && !(down))
-						state <= q_STILL;
-						
-						
-					if ( ((xpos <= shark1xpos+10)&& (xpos >= shark1xpos-10) && (ypos <= shark1ypos+10) && (ypos >= shark1ypos-10)) || ((xpos <= shark2xpos+10)&& (xpos >= shark2xpos-10) && (ypos <= shark2ypos+10) && (ypos >= shark2ypos-10))) 
-						state <= rst;
-					
-					if  ((xpos <= bottle1xpos+10)&& (xpos >= bottle1xpos-10) && (ypos <= bottle1ypos+10) && (ypos >= bottle1ypos-10)) 						
-						begin
-							bottleCount <= bottleCount + 1;
-							// assign bottle1 = ((hCount >= (bottle1xpos-0)) && (hCount <= (bottle1xpos+0))) && ((vCount >= (bottle1ypos-0)) && (vCount <= (bottle1ypos+0))) ? 1 : 0;
-						end
-
-					if  ((xpos <= bottle2xpos+10)&& (xpos >= bottle2xpos-10) && (ypos <= bottle2ypos+10) && (ypos >= bottle2ypos-10)) 						
-						begin
-							bottleCount <= bottleCount + 1;
-							// assign bottle2 = ((hCount >= (bottle2xpos-0)) && (hCount <= (bottle2xpos+0))) && ((vCount >= (bottle2ypos-0)) && (vCount <= (bottle2ypos+0))) ? 1 : 0;
-						end
-					
-					if (bottleCount == 3)
-						state <= q_DONE;	
-				end
-
-				q_UP:
-				begin
-					ypos<=ypos-2;
-					if (ypos==34)
-						ypos<=36;
-						
-					if (down)
-						state <= q_DOWN;
-					else if (up)
-						state <= q_UP;
-					else if (!(up) && !(down))
-						state <= q_STILL;
-						
-						
-					if ( ((xpos <= shark1xpos+10)&& (xpos >= shark1xpos-10) && (ypos <= shark1ypos+10) && (ypos >= shark1ypos-10)) || ((xpos <= shark2xpos+10)&& (xpos >= shark2xpos-10) && (ypos <= shark2ypos+10) && (ypos >= shark2ypos-10))) 
-						state <= rst;
-						
-					if  ((xpos <= bottle1xpos+10)&& (xpos >= bottle1xpos-10) && (ypos <= bottle1ypos+10) && (ypos >= bottle1ypos-10)) 						
-						begin
-							bottleCount <= bottleCount + 1;
-							// assign bottle1 = ((hCount >= (bottle1xpos-0)) && (hCount <= (bottle1xpos+0))) && ((vCount >= (bottle1ypos-0)) && (vCount <= (bottle1ypos+0))) ? 1 : 0;
-						end
-
-					if  ((xpos <= bottle2xpos+10)&& (xpos >= bottle2xpos-10) && (ypos <= bottle2ypos+10) && (ypos >= bottle2ypos-10)) 						
-						begin
-							bottleCount <= bottleCount + 1;
-							// assign bottle2 = ((hCount >= (bottle2xpos-0)) && (hCount <= (bottle2xpos+0))) && ((vCount >= (bottle2ypos-0)) && (vCount <= (bottle2ypos+0))) ? 1 : 0;
-						end
-					
-					if (bottleCount == 8)
-						state <= q_DONE;
-					
-				end
-				
-				q_DOWN:
-				begin
-					ypos<=ypos+2;
-					if(ypos==514)
-						ypos<=512;
-
-					if (down)
-						state <= q_DOWN;
-					else if (up)
-						state <= q_UP;
-					else if (!(up) && !(down))
-						state <= q_STILL;
-						
-						
-					if ( ((xpos <= shark1xpos+10)&& (xpos >= shark1xpos-10) && (ypos <= shark1ypos+10) && (ypos >= shark1ypos-10)) || ((xpos <= shark2xpos+10)&& (xpos >= shark2xpos-10) && (ypos <= shark2ypos+10) && (ypos >= shark2ypos-10))) 
-						state <= rst;
-					
-					if  ((xpos <= bottle1xpos+10)&& (xpos >= bottle1xpos-10) && (ypos <= bottle1ypos+10) && (ypos >= bottle1ypos-10)) 						
-						begin
-							bottleCount <= bottleCount + 1;
-							// assign bottle1 = ((hCount >= (bottle1xpos-0)) && (hCount <= (bottle1xpos+0))) && ((vCount >= (bottle1ypos-0)) && (vCount <= (bottle1ypos+0))) ? 1 : 0;
-						end
-
-					if  ((xpos <= bottle2xpos+10)&& (xpos >= bottle2xpos-10) && (ypos <= bottle2ypos+10) && (ypos >= bottle2ypos-10)) 						
-						begin
-							bottleCount <= bottleCount + 1;
-							// assign bottle2 = ((hCount >= (bottle2xpos-0)) && (hCount <= (bottle2xpos+0))) && ((vCount >= (bottle2ypos-0)) && (vCount <= (bottle2ypos+0))) ? 1 : 0;
-						end					
-					if (bottleCount == 8)
-						state <= q_DONE;
-						
-				end
-				
-				q_DONE:
-					background <= 12'b1111_1111_1111;
-			
-			endcase
-		end
-	end
-		
-	// 	 Note that the top left of the screen does NOT correlate to vCount=0 and hCount=0. The display_controller.v file has the 
-	// 		synchronizing pulses for both the horizontal sync and the vertical sync begin at vcount=0 and hcount=0. Recall that after 
-	// 		the length of the pulse, there is also a short period called the back porch before the display area begins. So effectively, 
-	// 		the top left corner corresponds to (hcount,vcount)~(144,35). Which means with a 640x480 resolution, the bottom right corner 
-	// 		corresponds to ~(783,515).  
-	// 	
-	
-	//the background color reflects the most recent button press
-	always@(posedge clk, posedge rst) begin
-		if(rst)
-			background <= 12'b0000_0000_0000; //white
-		else 
-			if(right)
-				background <= 12'b0000_1111_1111; //yellow
-			else if(left)
-				background <= 12'b0000_1111_1111; //turquoise
-			else if(down)
-				background <= 12'b1111_0000_1111; //green
-			else if(up)
-				background <= 12'b0000_1111_1111; //blue
-	end
-	
-endmodule
-*/
